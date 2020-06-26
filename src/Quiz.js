3f8a565a70de784c0ae01bc97a15a8c2a565f403
CiaranCheetham <ciarancheetham@hotmail.co.uk>
2020-06-26 16:37:30 +0100

    added quiz.js

---
+++ src/Quiz.js
@@ -0,0 +1,31 @@
+import React, { Component, useState} from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {quizPosition: 1};
+    }
+
+    render() {
+        return <div>
+            <div className='QuizQuestion'>
+                {quizData.quiz_questions[0].instruction_text}
+            </div>
+        </div>
+    }
+}
+
+// function Quiz() {
+//     const [quizPosition, setQuizPosition] = useState(1);
+//     return (
+//     <div>
+//         <div className="QuizQuestion">
+//             {quizData.quiz_questions[0].instruction_text}
+//         </div>
+//     </div>
+//     );
+// }
+
+export default Quiz;
