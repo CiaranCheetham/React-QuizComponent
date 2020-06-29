bfad62d63981c2fc8335e25a5f5a3a7fce64a501
CiaranCheetham <ciarancheetham@hotmail.co.uk>
2020-06-29 09:49:04 +0100

    Done next bit

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component, useState} from 'react';
 import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json');
 
@@ -10,21 +11,15 @@ class Quiz extends Component {
     }
 
     render() {
+        const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+
         return <div>
+            {isQuizEnd ? 
+            <QuizEnd/> :
             <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+            }
         </div>
     }
 }
-
-// function Quiz() {
-//     const [quizPosition, setQuizPosition] = useState(1);
-//     return (
-//     <div>
-//         <div className="QuizQuestion">
-//             {quizData.quiz_questions[0].instruction_text}
-//         </div>
-//     </div>
-//     );
-// }
 
 export default Quiz;
